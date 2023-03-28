ea537da49bed3fd9f3e88de3c19a4f3810f4ea7b
헤파리 <107000683+rosmontisu@users.noreply.github.com>
2023-03-28 15:21:18 +0900

    코딩 복귀 손풀기 문제

---
+++ Math/1977 perfect square.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+void input();
+void answer(int m, int n);
+
+int main()
+{
+    input();
+    return 0;
+}
+
+void input()
+{
+    int M, N;
+    cin >> M;
+    cin >> N;
+    answer(M, N);
+}
+
+void answer(int m, int n)
+{
+    int sum = 0;
+    int min = 10001;
+    
+    int i = 1;
+    for (i=1; i*i <= n; i++)
+    {
+        // 최대 최소 제한
+        if (i*i >= m && i*i <= n)
+        {
+            sum += i*i;
+            if (i*i < min)
+                min = i*i;
+        }
+    }
+    
+    if (sum == 0)
+        cout << -1 << endl;
+    else
+    {
+        cout << sum << endl;
+        cout << min << endl;
+    }
+}
+
