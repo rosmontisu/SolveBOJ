d36f66556cb2f9381f64da7bef43c1051ee62612
헤파리 <107000683+rosmontisu@users.noreply.github.com>
2023-04-25 16:21:50 +0900

    Create 5622.c

---
+++ bojStep/chapter5_string/5622.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+num1 = 2s
+num2 = 3s
+...
+numN = (N+1)s
+
+3개씩 단어 묶음
+65 66 67 -> 2
+1 2 3
+68 69 70 -> 3
+4 5 6
+71 72 73 -> 4
+7 8 9
+(문자 - 63)
+*/
+
+int main()
+{
+  char str[20];
+  
