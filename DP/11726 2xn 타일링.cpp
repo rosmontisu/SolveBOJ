b6f13c289906bbc8d8109e8b2017aeae5afb8aa0
hepari <107000683+rosmontisu@users.noreply.github.com>
2023-08-06 21:32:01 +0900

    Create 11726 2xn 타일링.cpp

---
+++ DP/11726 2xn 타일링.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int dp[10007];
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 2;
+
+    // 현재 dp번째 도형이 세로라고 가정했을때의 경우의 수와
+    // 현재 dp번째 도형이 가로라고 가정했을때의 경우의 수를
+    // 생각해보면 쉽게 풀린다.
+
+    for (int i = 3; i <= n; i++)
+    {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+    }
+
+    cout << dp[n];
+    return 0;
+}
