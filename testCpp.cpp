22b2429d289486075d47c45c117ad2ac6597de0c
hepari <107000683+rosmontisu@users.noreply.github.com>
2023-10-20 08:06:10 +0000

    dlt obj

---
+++ testCpp.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int func4(int n)
+{
+    int result = 0;
+
+    // 제곱수면 1
+    // 제곱수가 아니면 0
+    if (sqrt(n))
+
+
+    
+
+
+    return result;
+}
+
+int main(void)
+{
+    int input;
+    cin >> input;
+
+    func4()
+
+
+}
