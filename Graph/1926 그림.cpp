d8e3b212863d967ffb52a82685c7e78fc7068635
hepari <107000683+rosmontisu@users.noreply.github.com>
2023-11-16 14:48:32 +0900

    bfs 연습 문제 그림

---
+++ Graph/1926 그림.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+queue<pair<int, int>> Q;
+bool vis[502][502];
+int map[502][502];
+
+int n, m;
+
+int dx[4] = { -1, 0, 1, 0 };
+int dy[4] = { 0, 1, 0, -1 };
+
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> map[i][j];
+		}
+	}
+	//Q.push({ 0,0 });
+	//vis[0][0] = 1;
+	int maxArea = 0;
+	int num = 0;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (map[i][j] == 0 || vis[i][j]) continue;
+
+			num++; // 새로운 그림의 시작점을 찾음
+			vis[i][j] = 1;
+			Q.push({ i,j });
+			int area = 0;
+			while (!Q.empty()) {
+				area++;
+				pair<int, int> cur = Q.front(); Q.pop();
+				for (int dir = 0; dir < 4; dir++) {
+					int nowX = cur.first + dx[dir];
+					int nowY = cur.second + dy[dir];
+					if (nowX < 0 || nowY < 0 || nowX >= n || nowY >= m)
+						continue;
+					if (vis[nowX][nowY] || map[nowX][nowY] != 1)
+						continue;
+					Q.push({ nowX,nowY });
+					vis[nowX][nowY] = true;
+				}
+			}
+			if (area > maxArea)
+				maxArea = area;
+		}
+	}
+
+	cout << num << "\n";
+	cout << maxArea;
+}
