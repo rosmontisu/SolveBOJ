be682af8471a6e638414c2955ed20410eb3b6693
hepari <107000683+rosmontisu@users.noreply.github.com>
2024-01-08 17:47:49 +0900

    Create 1780 종이의 개수.cpp

---
+++ Recursion/1780 종이의 개수.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n; // 1 <= n <= 3^7
+int paper[2188][2188]; // n의 최대값은 2187
+int dpos[3] = { -1, 0, 1 };
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n;
+	return 0;
+}
