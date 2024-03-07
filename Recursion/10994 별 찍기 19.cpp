d63bc7a2b401ce67eaf715edaceee20f5f4bffab
hepari <107000683+rosmontisu@users.noreply.github.com>
2024-03-07 15:23:04 +0900

    하변 우변의 x y 좌표 보정을 잊지 말자

---
+++ Recursion/10994 별 찍기 19.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+char map[400][400];
+
+void Fractal(int x, int y, int n) {
+	if (n == 1) {
+		map[x][y] = '*';
+		return;
+	}
+
+	int length = 4 * n - 3;
+
+	for (int i = 0; i < length; i++) {
+		map[x][y+i] = '*'; // 상변
+		map[x+i][y] = '*'; // 좌변
+		map[x+length - 1][y+i] = '*'; // 하변
+		map[x+i][y+length - 1] = '*'; // 우변
+	}
+
+	Fractal(x + 2, y + 2, n - 1);
+}
+
+int main(void) {
+	int n; cin >> n;
+	int length = 4 * n - 3;
+	for (int i = 0; i < length; i++)
+		for (int j = 0; j < length; j++)
+			map[i][j] = ' ';
+	
+	Fractal(0, 0, n);
+	for (int i = 0; i < length; i++) {
+		for (int j = 0; j < length; j++)
+			cout << map[i][j];
+		cout << '\n';
+	}
+		
+	return 0;
+}
