aae0482e4c4f78216a212c1ff09d66ee1bc59b8b
rosmontisu <rosmontisu@gmail.com>
2024-04-25 17:49:08 +0900

    add 11403 경로찾기.cpp

---
+++ Graph/BFS-DFS/11403 경로 찾기.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+using namespace std;
+
+/*
+가중치가 없는 방향그래프 문제 
+인접 리스트 형식으로 데이터를 저장
+DFS로 풀어냅시다
+*/
+
+void dfs();
+
+int n;
+int i, j;
+
+vector<int> edj[102];
+
+int main(void)
+{
+	cin >> n;
+	while (n--)
+	{
+		cin >> i >> j;
+
+	}
+}
