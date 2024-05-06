00c4a0cc411ece82350aa42b54de90c6d8edb0eb
hepari <107000683+rosmontisu@users.noreply.github.com>
2024-05-06 15:47:27 +0900

    실험용

--- Implementation/실험용.cpp
+++ Implementation/실험용.cpp
@@ -1,26 +1,18 @@
 #include <iostream>
 #include <string>
+#include <math.h>
 using namespace std;
 
-int flr[10003];
 
 int main(void)
 {
-	int f, n;
-	cin >> f >> n; // 아파트 층수, 참가자의 수
-	
-	// flr[n] 배열에는 n번째 층수의 사람의 번호가 저장된다.
-	// 1사람은 2개의 손을 가지므로, 참가자의 수 m*2 만큼의 층수가 저장된다.
-	
-	for (int i = 1; i <= n; i++)
-	{
-	    int n1, n2; // 손층수 1, 손층수 2
-	    cin >> n1 >> n2;
-	    flr[n1] = i;
-	    flr[n2] = i;
-	}
-    
-    int targetFlr = f % (2 * n);
-    if (targetFlr == 0) targetFlr = 2 * n; // 0이면 가장 마지막 손 위치
-    cout << flr[targetFlr];
+	int a, b, c;
+	cin >> a >> b >> c;
+
+	if (a == 0)
+		cout << (c*c) - b;
+	else if (b == 0)
+		cout << (c*c) - a;
+	else if (c == 0)
+		cout << sqrt(a+b);
 }
