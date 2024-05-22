132689b36d844bbf0825b30abcc37d96eb217509
rosmontisu <rosmontisu@gmail.com>
2024-05-22 14:23:14 +0900

    벽 부수고 이동하기 문제 해결

---
+++ Graph/BFS-DFS/2206 벽 부수고 이동하기-시간초과.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int n, m;
+int map[1002][1002];
+int map_copy[1002][1002];
+int dist[1002][1002];
+int dx[4] = { 0, 1, 0, -1 };
+int dy[4] = { 1, 0, -1, 0 };
+int answer = 1000002;
+
+/*
+벽 1개가 제거된 모든 경우의 수에서 BFS를 돌린 후, 최단 경로를 서치?
+*/
+
+// 배열을 모두 초기화 시켜줍니다
+void Init()
+{
+    
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            map_copy[i][j] = map[i][j];
+            dist[i][j] = 0;
+        } 
+    }
+}
+
+void Bfs()
+{
+    queue<pair<int, int>> q;
+    q.push({ 1, 1 });
+    dist[1][1] = 1; // 항상 시작점을 거리 1로 계산합니다.
+    while(!q.empty())
+    {
+        pair<int, int> cur = q.front(); q.pop();
+        for (int dir = 0; dir < 4; dir++)
+        {
+            int nx = cur.first + dx[dir];
+            int ny = cur.second + dy[dir];
+
+            if (nx < 1 || ny < 1 || nx > n || ny > m) continue; // 범위 설정 잘하기 <- 여기서 오류가 났던것
+            if (map_copy[nx][ny] == 1 || dist[nx][ny] > 0) continue;     
+
+            dist[nx][ny] = dist[cur.first][cur.second] + 1;
+            q.push({ nx, ny });
+            if (nx == n && ny == m)
+                answer = min(answer, dist[nx][ny]);
+        }
+
+    }
+}
+void Answer()
+{  
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            if (map[i][j])
+            {
+                Init(); 
+                map_copy[i][j] = 0; // 벽을 허물고 
+                Bfs(); // 검사 
+            }
+        }
+    }
+    Init(); 
+    Bfs(); // 벽을 허물지 않는 경우도 1번 검사
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++)
+    {
+        string s;
+        cin >> s;
+        for (int j = 0; j <= m; j++)
+            map[i][j+1] = s[j] - '0';
+    }
+    Answer();
+    if (answer < 1000002)
+        cout << answer;
+    else
+        cout << -1; 
+}
--- Graph/BFS-DFS/2206 벽 부수고 이동하기.cpp
+++ Graph/BFS-DFS/2206 벽 부수고 이동하기.cpp
@@ -1,91 +1,84 @@
 #include <iostream>
-#include <queue>
+#include <queue> 
+#include <tuple> 
 using namespace std;
+/*
+첫 번째 풀이처럼 벽이 부서진 모든 경우의 수로 풀면
+시간 초과가 발생
+시간복잡도가 대략 O(n^2 * m^2) 
+엄~청 느리다!
 
+고로, 벽을 만날때만 벽을 허무는 연산을 수행하도록 코드를 수정해보자
+- dist[] 배열을 3차원으로 수정해서, 벽이 부시기전까지의 거리를 측정하다가, 벽을 부순 후 다른 차원에서 이어서 거리를 계산
+    dist[x][y][0]은 벽이 있는 상태 에서의 최단 거리
+    dist[x][y][1]은 벽을 허문 상태 에서의 최단 거리
+
+    dist[x][y][0]상태에서 벽을 만나면 벽을 허물고 연산을 수행하도록 해보자
+*/
 int n, m;
 int map[1002][1002];
-int map_copy[1002][1002];
-int dist[1002][1002];
+int dist[1002][1002][2];
+
 int dx[4] = { 0, 1, 0, -1 };
 int dy[4] = { 1, 0, -1, 0 };
-int answer = 1000002;
-
-/*
-벽 1개가 제거된 모든 경우의 수에서 BFS를 돌린 후, 최단 경로를 서치?
-*/
-
-// 배열을 모두 초기화 시켜줍니다
-void Init()
-{
-    
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= m; j++)
-        {
-            map_copy[i][j] = map[i][j];
-            dist[i][j] = 0;
-        } 
-    }
-}
-
 void Bfs()
 {
-    queue<pair<int, int>> q;
-    q.push({ 1, 1 });
-    dist[1][1] = 1; // 항상 시작점을 거리 1로 계산합니다.
-    while(!q.empty())
+    queue<tuple<int, int, int>> q; // (x, y, wall_break_status) 
+    q.push({1, 1, 0});
+    dist[1][1][0] = 1;
+    while (!q.empty())
     {
-        pair<int, int> cur = q.front(); q.pop();
+        int x, y, wall_chance;
+        tie(x, y, wall_chance) = q.front(); q.pop();
         for (int dir = 0; dir < 4; dir++)
         {
-            int nx = cur.first + dx[dir];
-            int ny = cur.second + dy[dir];
+            int nx = x + dx[dir];
+            int ny = y + dy[dir];
+            int distance = dist[x][y][wall_chance] + 1;
 
-            if (nx < 1 || ny < 1 || nx > n || ny > m) continue; // 범위 설정 잘하기 <- 여기서 오류가 났던것
-            if (map_copy[nx][ny] == 1 || dist[nx][ny] > 0) continue;     
+            if (nx < 1 || ny < 1 || nx > n || ny > m) continue; // 범위 체크 
+            // 벽을 만나지 않았고, 처음 방문했는데, 이전보다 효율적인 거리라면
+            if (map[nx][ny] == 0 && dist[nx][ny][wall_chance] <= 0)
+            {      
+                dist[nx][ny][wall_chance] = distance;
+                q.push({nx, ny, wall_chance}); 
+            } 
 
-            dist[nx][ny] = dist[cur.first][cur.second] + 1;
-            q.push({ nx, ny });
-            if (nx == n && ny == m)
-                answer = min(answer, dist[nx][ny]);
-        }
-
-    }
-}
-void Answer()
-{  
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= m; j++)
-        {
-            if (map[i][j])
-            {
-                Init(); 
-                map_copy[i][j] = 0; // 벽을 허물고 
-                Bfs(); // 검사 
-            }
-        }
-    }
-    Init(); 
-    Bfs(); // 벽을 허물지 않는 경우도 1번 검사
-}
-
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-
-    cin >> n >> m;
-    for (int i = 1; i <= n; i++)
-    {
-        string s;
-        cin >> s;
-        for (int j = 0; j <= m; j++)
+            // 벽을 만났고, 처음 방문했고, 벽을 부실 기회가 있다면
+            // 벽을 부순 차원으로 이동해서 PUSH
+            if (map[nx][ny] == 1 && dist[nx][ny][wall_chance] <= 0 && wall_chance == 0)
+            { 
+                dist[nx][ny][1] = distance; 
+                q.push({nx, ny, 1}); 
+            } 
+        } 
+    } 
+} 
+ 
+int main() 
+{ 
+    ios::sync_with_stdio(false); 
+    cin.tie(0); 
+ 
+    cin >> n >> m; 
+    for (int i = 1; i <= n; i++) 
+    { 
+        string s; 
+        cin >> s; 
+        for (int j = 0; j < m; j++) 
             map[i][j+1] = s[j] - '0';
-    }
-    Answer();
-    if (answer < 1000002)
-        cout << answer;
+    } 
+    dist[n][m][0] = -1;
+    dist[n][m][1] = -1;
+
+    Bfs(); 
+    
+    // 둘 중 하나가 음수면, 양수인 값을
+    // 둘 모두 음수/양수면 가장 작은 값을
+    if(dist[n][m][0] * dist[n][m][1] < 0)
+        cout << max(dist[n][m][0], dist[n][m][1]); 
     else
-        cout << -1; 
+        cout << min(dist[n][m][0], dist[n][m][1]); 
+ 
+    return 0; 
 }
