6745a2341f5725c4bbe5ddb20a83d03efeac10aa
rosmontisu <rosmontisu@gmail.com>
2024-06-18 16:08:24 +0900

    첫 위상정렬 문제 풀이 - 2252 줄 세우기 - indegree == 0 인 경우를 찾는 방식으로 구현 - bfs와 유사한 구조, 구현이 어렵지 않다! - vector<T> adj[n] 으로 그래프를 구현하는 방식을 잊지 말기 - 시간 복잡도 O(V+E) - (그래프로 구현하는 bfs/dfs와 시간복잡도의 원리가 똑같다)

---
+++ Graph/TopologySort (위상정렬)/2252 줄 세우기.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int degree[100001]; // 각 정점의 indegree 수를 저장
+vector<int> adj[100001]; // my graph
+queue<int> q; // indegree == 0 저장용 큐
+
+vector<int> result; // 정답 출력용
+
+int main()
+{
+    int n, m; // 학생수, 비교수
+    cin >> n >> m;
+
+    // m회 비교
+    for (int i = 1; i <= m; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        adj[a].push_back(b);
+        degree[b]++; // b는 indegree증가
+    }
+
+    for (int i = 1; i <= n; i++)
+        if (degree[i] == 0) q.push(i);
+
+    // 위상 정렬 알고리즘 시작
+    // bfs와 유사한 과정임을 볼 수 있다.
+    while (!q.empty())
+    {
+        int cur = q.front(); q.pop();
+        result.push_back(cur); // 위상 정렬
+        for (int nxt : adj[cur])
+        {
+            degree[nxt]--;
+            if (degree[nxt] == 0) q.push(nxt);
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << result[i] << ' ';
+    }
+
+    return 0;
+}
