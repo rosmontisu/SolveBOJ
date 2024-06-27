7e54944b10935a19d425165493e60bfbcd9c9e77
rosmontisu <rosmontisu@gmail.com>
2024-06-27 17:37:56 +0900

    n과 m 풀던거 중간 저장

---
+++ Graph/TopologySort (위상정렬)/1766 문제집.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> adj[40000];
+int indegree[40000];
+
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n, m; // 문제의 수, 정보의 수
+    for (int i = 0; i < m; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        adj[a]
+    }
+}
