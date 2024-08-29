6c8a9a1649db08e1fb2002c5e20c60504bea4499
rosmontisu <rosmontisu@gmail.com>
2024-08-29 17:22:20 +0900

    별찍기 8 풀던거..

---
+++ 기초 코드 작성 요령 2/2445 별 찍기 8.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = n-i-1; j < n; j++)
+        {
+            cout << '*';
+            n-j 
+        }
+
+        
+        cout << '\n';
+    }
+
+    return 0;
+}
