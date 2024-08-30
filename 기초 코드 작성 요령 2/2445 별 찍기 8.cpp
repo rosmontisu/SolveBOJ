e18fedb69c6562a359ced46e4263f3f2c4ed1570
rosmontisu <rosmontisu@gmail.com>
2024-08-30 13:46:31 +0900

    별찍기 8번, 통이 아니라 출력 하나하나 작게 쪼개서 구현해밧음

--- 기초 코드 작성 요령 2/2445 별 찍기 8.cpp
+++ 기초 코드 작성 요령 2/2445 별 찍기 8.cpp
@@ -1,22 +1,55 @@
 #include <iostream>
 using namespace std;
+int n;
+
+
+void topStar(int i)
+{
+    for (int j = n-i-1; j < n; j++)
+        cout << '*';
+}
+void topSpace(int i)
+{
+    for (int j = i + 1; j < n; j++)
+        cout << ' ';
+}
+
+void underStar(int i)
+{
+    for (int j = i; j < n-1; j++)
+        cout << '*';
+}
+void underSpace(int i)
+{
+    for (int j = n-i-1; j < n; j++)
+        cout << ' ';
+}
+
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    int n;
+    
     cin >> n;
 
+    // 위부터 중앙까지
     for (int i = 0; i < n; i++)
     {
-        for (int j = n-i-1; j < n; j++)
-        {
-            cout << '*';
-            n-j 
-        }
+        topStar(i);
+        topSpace(i);
+        topSpace(i);
+        topStar(i);
+        cout << '\n';
+    }
 
-        
+    // 아래만
+    for (int i = 0; i < n-1; i++)
+    {
+        underStar(i);
+        underSpace(i);
+        underSpace(i);
+        underStar(i);
         cout << '\n';
     }
 
