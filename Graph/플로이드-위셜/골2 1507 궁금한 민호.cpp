ecf8be7382cecfb32dfa806754880d84479ed84a
hepari-office <rosmontisu@gmail.com>
2025-02-03 11:56:33 +0900

    .

--- Graph/플로이드-위셜/골2 1507 궁금한 민호.cpp
+++ Graph/플로이드-위셜/골2 1507 궁금한 민호.cpp
@@ -7,5 +7,5 @@ int main()
     cin.tie(0);
     ios::sync_with_stdio(0);
 
-    
+    int n;
 }
---
+++ Math/골5 1484 다이어트.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false); 
+    cin.tie(NULL);
+    
+    int g; 
+    cin >> g;
+
+    /*
+    g = now^2 - before^2
+
+    // g는 주어지고, now를 찾아야하므로
+
+    before^2 = now^2 - g 
+    (단, now >= before)
+    (before은 자연수여야 함)
+    */
+
+   for (int i = )
+
+
+    
+    return 0;
+}
