37ba9ccf538dbdd5563115ff53e3cef2eae3d732
hepari <rosmontisu@gmail.com>
2025-06-11 15:42:51 +0900

    배경용 문제

---
+++ Implementation/브4 29790 임스의 메이플컵.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int n, u, l;
+    cin >> n >> u >> l;
+
+    if (n >= 1000)
+    {
+        if (u >= 8000 || l >= 260)
+            cout << "Very Good";
+        else
+            cout << "Good";
+    }
+    else
+        cout << "Bad";
+}
