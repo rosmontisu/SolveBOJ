d321cb13ae7b80928b53f760ff273b410ac41826
hepari <107000683+rosmontisu@users.noreply.github.com>
2025-09-07 17:36:14 +0900

    투포인터 쉬운거 하나 스윽윽

---
+++ TwoPointer/플5 14572 스터디 그룹.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int n, k, d;
+    cin >> n >> k >> d;
+
+    // 가장 잘하는 학생 vs 못하는 학생 실력차 D이하
+
+    // E = (아는 알고리즘 수 - 모두가 아는 알고리즘 수) * 그룹원수
+}
