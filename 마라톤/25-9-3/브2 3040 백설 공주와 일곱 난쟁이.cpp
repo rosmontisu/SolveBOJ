1e97fd6ff15722be89f52e21823e1d6028a2822a
hepari <107000683+rosmontisu@users.noreply.github.com>
2025-09-19 19:24:54 +0900

    인자가 9개니 그냥 n^2

---
+++ 마라톤/25-9-3/브2 3040 백설 공주와 일곱 난쟁이.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int sum = 0;
+    vector<int> v(9);
+    for (int i = 0; i < 9; i++)
+    {
+        cin >> v[i];
+        sum += v[i];
+    }
+
+    for (int i = 0; i < 9; i++)
+    {
+        for (int j = i + 1; j < 9; j++)
+        {
+            if (sum - v[i] - v[j] == 100)
+            {
+                v[i] = 0;
+                v[j] = 0;
+
+                for (int k = 0; k < 9; k++)
+                    if (v[k] != 0) cout << v[k] << '\n';
+                    
+                return 0;
+            }
+        }
+    }
+}
