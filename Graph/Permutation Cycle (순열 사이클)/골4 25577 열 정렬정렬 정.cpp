e3cf3b339a0491f3da9989d27e233d5205e00c83
hepari <rosmontisu@gmail.com>
2026-02-17 15:44:29 +0900

    x 아직 안품품

---
+++ Graph/Permutation Cycle (순열 사이클)/골4 25577 열 정렬정렬 정.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n; cin >> n;
+    vector<int> v(n * 2 + 1, -1);
+    vector<bool> vis(n * 2 + 1, false);
+
+    for (int i = 1; i <= n; i++)
+    {
+        
+    }
+
+}
